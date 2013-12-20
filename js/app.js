ff0c9a19beb89df9ed8731ff3356e33f2bde0256
Mec-iS <tunedconsulting@gmail.com>
2013-12-20 17:28:21 +0100

    cambiamenti minori

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('lqfb-stats', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'lqfb-stats.filters',
+  'lqfb-stats.services',
+  'lqfb-stats.directives',
+  'lqfb-stats.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/viewActive', {templateUrl: 'partials/active.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/viewTimeLine', {templateUrl: 'partials/timeline.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+angular.module('lqfb-stats.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
