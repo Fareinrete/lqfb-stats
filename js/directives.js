4621f913319c3065dbba6a08f107244b5351300c
Mec-iS <lorenz.mh@gmail.com>
2013-12-23 01:38:33 +0000

    aggiunta directive

--- js/directives.js
+++ js/directives.js
@@ -17,47 +17,47 @@ directives
 				values: '='
 			},
 			link: function (scope, element, attrs) {
-				 scope.$watch('values', function(data) {
-                                        if(data) { 
-			
+				    scope.$watch('values', function(data) {
+                      if(data) { 
 						console.log('values from directive: ', data); 
-						
+			 	
 						var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2;
+                        height = 500,
+                        radius = Math.min(width, height) / 2;
 
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+                        var color = d3.scale.ordinal()
+                                    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
+                        var arc = d3.svg.arc()
+                             .outerRadius(radius - 10)
+                             .innerRadius(4);
 
-var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d.user_count; });
+                        var pie = d3.layout.pie()
+                             .sort(null)
+                             .value(function(d) { return d.user_count; });
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+                        var svg = d3.select("body").append("svg")
+                                      .attr("width", width)
+                                      .attr("height", height)
+                                      .append("g")
+                                      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  var g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
+                       var g = svg.selectAll(".arc")
+                                      .data(pie(data))
+                                      .enter().append("g")
+                                      .attr("class", "arc");
 
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.status); });
+                           g.append("path")
+                                      .attr("d", arc)
+                                      .style("fill", function(d) { 
+                                          return color(d.data.status); 
+                                      });
 
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.status; });
-
+                           g.append("text")
+                                      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+                                      .attr("dy", ".35em")
+                                      .style("text-anchor", "middle")
+                                      .text(function(d) { return d.data.status; });
 					}
 				 });
 				}
