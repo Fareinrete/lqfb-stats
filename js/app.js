ae1397efbe9ee930a28f7f94f269d5ac305dca8a
Mec-iS <lorenz.mh@gmail.com>
2014-01-13 14:08:22 +0000

    testing endpoints

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //load 
 //'http',
 var ENDPOINTS = 
-          { activated:       'http://vs1.proposte.fermareildeclino.it/~fale/activated.json',
+          { activated:       'http://api.fareinrete.org/v1/users/activated?callback=JSON_CALLBACK',
             tl_activations:  'http://api.fareinrete.org/v1/users/activations?callback=JSON_CALLBACK',
             tl_activated:    'http://api.fareinrete.org/v1/users/lastlogin?callback=JSON_CALLBACK',
             admin_activated: 'http://api.fareinrete.org/v1/admins/activated?callback=JSON_CALLBACK'
